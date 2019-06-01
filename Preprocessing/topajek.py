4986c6620f2c0d3fd800211497d06b8c37c42e88
Apratim Das <apratimdas@gmail.com>
2019-05-31 18:52:49 -0700

    updated data

--- Preprocessing/topajek.py
+++ Preprocessing/topajek.py
@@ -3,19 +3,19 @@ import re
 
 flag = False
 
-print("*Vertices 120")
-for i in range(0,120):
+print("*Vertices 55")
+for i in range(0,55):
     print(i,"\"" + str(i) + "\"")
 
 print()
 print("*arcs")
 
-with open("signedgenerated10_2.txt", "r") as ins:
+with open("watershed-s-directedgraph.txt", "r") as ins:
     for line in ins:
         if not flag:
             flag = True
             continue
 
         temp = line[:-1].split(" ")
-        temp.pop(-1)
+        # temp.pop(-1)
         print(temp[0],temp[1])
