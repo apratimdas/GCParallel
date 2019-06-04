021f2854d89188a675f38b2bd742d3fc65b86b97
Apratim Das <apratimdas@gmail.com>
2019-06-04 13:59:32 -0700

    test

---
+++ Preprocessing/correlationmatrixgenerator.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+np.random.seed(1)
+
+# 1000 random integers between 0 and 50
+x = np.random.randint(0, 50, 1000)
+
+# Positive Correlation with some noise
+y = x + np.random.normal(0, 10, 1000)
+
+
+data = []
+data.append(x)
+data.append(y)
+data.append(y)
+
+
+
+print(np.corrcoef(data))
